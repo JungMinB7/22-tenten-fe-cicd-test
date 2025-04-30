34a7b126ace9920f56694cfea4f67aa1dd6f3db7
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-04-30 19:55:39 +0900

    [FEAT] 헤더 구현 완료
    ## :heavy_check_mark: 체크리스트
    - [x] 디폴트 헤더 구현 완료
    - [ ] 페이지별 취소 렌더링 다른 거는 추후 수정 예정
    - [x] 로그인 페이지에 헤더 적용
    - [x] 채팅 목록 페이지에 헤더 적용

--- kakaobase/src/components/common/Header.tsx
+++ kakaobase/src/components/common/Header.tsx
@@ -1,7 +1,21 @@
-export default function Header() {
+'use client';
+import { Undo2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+export default function Header({ label }: { label: string }) {
+  const router = useRouter();
+
   return (
-    <div>
-      <div>헤더 컴포넌트</div>
+    <div className="h-20 px-10 flex relative text-textColor z-50">
+      <Undo2
+        onClick={() => {
+          router.back();
+        }}
+        className="cursor-pointer self-center"
+      />
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-2xl">
+        {label}
+      </div>
     </div>
   );
 }
