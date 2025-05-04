4b290f37a57bf0b14c4630738fd5028dceb955d8
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-05 02:11:28 +0900

    [FEAT] 유튜브 임베딩
    ## 변경 사항
    - [x] post card : 유튜브 임베딩 해결
    - [x] 백엔드에게 유튜브 아이디로 반환해 달라고 요청 완료
    
    ## 해결해야 할 부분
    - [ ] hook을 적용해서 좀더 동적인 처리로 바꿔야 함

--- kakaobase/src/components/post/PostCard.tsx
+++ kakaobase/src/components/post/PostCard.tsx
@@ -3,10 +3,18 @@ import { Heart, MessageCircle, ShieldAlert, User } from 'lucide-react';
 import FollowButtonSmall from '../user/FollowButtonSmall';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 export default function PostCard() {
   const [isLiked, setLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(12);
+  const youtubeId = 'GLSzQceXAbI';
+  const profileUrl = '/test_profile.jpg';
+  const imageUrl = '/test_profile.jpg';
+  const content =
+    '크리처보다는 도비죠. 아하하하하하하(아무튼 실성도비) 내용을 두 줄보다 길게 작성해 볼까나~~';
+  const nickname = 'daisy.kim';
+  const createdAt = '오전 11:24';
 
   function controlLike() {
     setLiked((prev) => !prev);
@@ -20,13 +28,23 @@ export default function PostCard() {
   const router = useRouter();
 
   return (
-    <div className="flex">
+    <div className="flex animate-slide-in">
       <div className="flex w-full bg-containerColor mx-8 my-4 p-4 gap-2 rounded-2xl">
         <div
-          className="flex w-8 h-8 rounded-lg bg-innerContainerColor justify-center items-center cursor-pointer"
+          className="flex w-8 h-7 rounded-lg bg-innerContainerColor justify-center items-center cursor-pointer"
           onClick={() => router.push('/profile/1')}
         >
-          <User className="text-textColor" />
+          {profileUrl ? (
+            <Image
+              src={profileUrl}
+              width={32}
+              height={32}
+              alt="프로필"
+              className="rounded-lg"
+            />
+          ) : (
+            <User className="text-textColor" width={20} height={20} />
+          )}
         </div>
         <div className="w-full flex flex-col gap-2 text-textColor">
           <div className="flex justify-between">
@@ -35,12 +53,12 @@ export default function PostCard() {
                 className="cursor-pointer"
                 onClick={() => router.push('/profile/1')}
               >
-                daisy.kim
+                {nickname}
               </div>
               <FollowButtonSmall />
             </div>
             <div className="flex gap-2 align-center justify-center">
-              <div className="flex self-center text-xs">오전 11:24</div>
+              <div className="flex self-center text-xs">{createdAt}</div>
               <ShieldAlert
                 width={16}
                 height={16}
@@ -53,13 +71,26 @@ export default function PostCard() {
             className="w-full text-sm line-clamp-2 overflow-hidden text-ellipsis cursor-pointer"
             onClick={() => router.push('post/[id]')}
           >
-            크리처보다는 도비죠. 아하하하하하하(아무튼 실성도비) 내용을 두
-            줄보다 길게 작성해 볼까나~~
+            {content}
           </div>
-          <div className="w-full h-40 bg-innerContainerColor rounded-lg text-center content-center">
-            image or youtube
-            <br />
-            [full x 160]
+          <div className="w-64 flex justify-center content-center">
+            {youtubeId === null ? (
+              <Image
+                src={imageUrl}
+                alt="이미지"
+                height={144}
+                width={152}
+                className="rounded-lg"
+              />
+            ) : (
+              <iframe
+                width="256"
+                height="144"
+                src={`https://www.youtube.com/embed/${youtubeId}`}
+                title="유튜브 영상"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              ></iframe>
+            )}
           </div>
           <div className="flex gap-4 text-sm">
             <div className="flex gap-1 justify-center">
