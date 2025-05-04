690991505de71e92fbdb140968aa3f5a38c2fd05
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-05 02:08:43 +0900

    [FIX] 메인 페이지 스크롤 문제 해결
    - [x] nav bar도 post card들과 함께 스크롤 되는 문제 해결

--- kakaobase/src/app/page.tsx
+++ kakaobase/src/app/page.tsx
@@ -4,9 +4,9 @@ import PostCard from '@/components/post/PostCard';
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col h-screen">
       <HeaderMain />
-      <div className="flex flex-col bg-scroll">
+      <div className="flex flex-col overflow-y-auto pb-20">
         <PostCard />
         <PostCard />
         <PostCard />
