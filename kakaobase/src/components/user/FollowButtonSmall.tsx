c22cb6c0110baf7005c77ee17a6fa24337cf1bfe
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-05 17:00:44 +0900

    [FEAT] 팔로우 버튼 훅 분리
    - 팔로우 버튼 상태관리 훅으로 분리
    - api 작성해야 됨

--- kakaobase/src/components/user/FollowButtonSmall.tsx
+++ kakaobase/src/components/user/FollowButtonSmall.tsx
@@ -1,24 +1,22 @@
-'use client';
-import { useState } from 'react';
+import { useFollowToggle } from '@/hooks/useFollowHook';
 
-export default function FollowButtonSmall() {
-  const [isFollowed, setFollowed] = useState(false);
-
-  function controlFollow() {
-    setFollowed((prev) => !prev);
-  }
-
+export default function FollowButtonSmall({
+  isFollowing,
+}: {
+  isFollowing: boolean;
+}) {
+  const { following, toggleFollow } = useFollowToggle(isFollowing);
   return (
     <button
-      onClick={controlFollow}
+      onClick={toggleFollow}
       className={`h-4 w-12 align-center rounded-full flex justify-center ${
-        !isFollowed
+        !following
           ? 'bg-myLightBlue text-textOnLight'
           : 'bg-myBlue text-textOnBlue'
       }`}
     >
       <div className="flex justify-self-center text-[0.625rem]">
-        {isFollowed ? '팔로우' : '언팔로우'}
+        {following ? '팔로우' : '언팔로우'}
       </div>
     </button>
   );
