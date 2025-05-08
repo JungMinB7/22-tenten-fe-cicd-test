68fe4ad3d4ecb7e37853ebf13743a43ba90d1740
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-08 15:06:13 +0900

    [FIX] 배포 문제 해결
    - next.config.mjs
    - 게시글 작성 페이지 -> csr

--- kakaobase/next.config.mjs
+++ kakaobase/next.config.mjs
@@ -1,13 +1,4 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // 개발 시 오류 감지 강화
-  swcMinify: true, // 빌드 최적화 (기본값 true)
-  experimental: {
-    appDir: true, // ✅ src/app 사용 시 필수
-  },
-  output: 'standalone', // Docker 또는 pm2 배포 시 권장
-};
+const nextConfig = {};
 
 export default nextConfig;
