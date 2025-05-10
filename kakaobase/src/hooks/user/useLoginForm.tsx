f87b653c9cdfc5fad58c8ae5ec9d3946080fe204
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-11 05:00:08 +0900

    [FIX] 로그인 api 성공
    - useLoginForm 훅 파일에서 주석이었던 거 제거

--- kakaobase/src/hooks/user/useLoginForm.tsx
+++ kakaobase/src/hooks/user/useLoginForm.tsx
@@ -3,15 +3,15 @@ import { loginSchema } from '@/schemas/loginSchema';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
-// import { useUserStore } from '@/stores/userStore';
-// import { v4 as uuidv4 } from 'uuid';
-// import login from '@/apis/login';
+import { login } from '@/apis/login';
+import { useUserStore } from '@/stores/userStore';
+import { v4 as uuidv4 } from 'uuid';
 
 type LoginFormData = z.infer<typeof loginSchema>;
 
 export default function useLoginForm() {
   const router = useRouter();
-  //const setUserInfo = useUserStore((state) => state.setUserInfo);
+  const setUserInfo = useUserStore((state) => state.setUserInfo);
 
   const loginForm = useForm<LoginFormData>({
     resolver: zodResolver(loginSchema),
@@ -24,38 +24,37 @@ export default function useLoginForm() {
   });
 
   const onSubmit = async (data: LoginFormData, autoLogin: boolean) => {
-    // const deviceId = localStorage.getItem('deviceId') || uuidv4();
-    // localStorage.setItem('deviceId', deviceId);
-    // const userAgent = navigator.userAgent;
+    const deviceId = localStorage.getItem('deviceId') || uuidv4();
+    localStorage.setItem('deviceId', deviceId);
+    const userAgent = navigator.userAgent;
 
-    // const requestBody = {
-    //   email: data.email,
-    //   password: data.password,
-    //   device_id: deviceId,
-    //   user_agent: userAgent,
-    // };
+    const requestBody = {
+      email: data.email,
+      password: data.password,
+      device_id: deviceId,
+      user_agent: userAgent,
+    };
 
-    // try {
-    //   const response = await login(requestBody);
-    //   document.cookie = `accessToken=${response.data.access_token}; path=/; secure; samesite=strict; max-age=3600`;
-    //   document.cookie = `course=${response.data.class_name}; path=/; max-age=86400`; //refresh Token이랑 기간 동일하게 하기
-    //   document.cookie = `course=${response.data.nickname}; path=/; max-age=86400`; //refresh Token이랑 기간 동일하게 하기
-    //   document.cookie = `course=${response.data.nickname}; path=/; max-age=86400`; //refresh Token이랑 기간 동일하게 하기
-    //   if (autoLogin) {
-    //     document.cookie = `autoLogin=true; path=/; max-age=86400`; //refresh Token이랑 기간 동일하게 하기
-    //   } else {
-    //     document.cookie = `autoLogin=false; path=/; max-age=0`; // 삭제
-    //   }
-    //   setUserInfo({
-    //     course: response.data.class_name,
-    //     nickname: response.data.nickname,
-    //     autoLogin: autoLogin,
-    //   });
-    //   router.push('/');
-    // } catch (e) {
-    //   console.log(e);
-    // }
-    router.push('/'); // 실제 api 연결 후 지우기
+    try {
+      const response = await login(requestBody);
+      document.cookie = `accessToken=${response.data.access_token}; path=/; secure; samesite=strict; max-age=3600`;
+      document.cookie = `course=${response.data.class_name}; path=/; max-age=86400`; //refresh Token이랑 기간 동일하게 or 1일
+      document.cookie = `course=${response.data.nickname}; path=/; max-age=86400`; //refresh Token이랑 기간 동일하게 하기 or 1일
+      document.cookie = `course=${response.data.nickname}; path=/; max-age=86400`; //refresh Token이랑 기간 동일하게 하기 or 1일
+      if (autoLogin) {
+        document.cookie = `autoLogin=true; path=/; max-age=86400`; //refresh Token이랑 기간 동일하게 하기 or 1일
+      } else {
+        document.cookie = `autoLogin=false; path=/; max-age=0`; // 삭제
+      }
+      setUserInfo({
+        course: response.data.class_name,
+        nickname: response.data.nickname,
+        autoLogin: autoLogin,
+      });
+      router.push('/');
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return {
