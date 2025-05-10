45a3574981ee970768d7b5d2758ea003dd278e8f
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-11 04:25:25 +0900

    [FIX] 비밀번호 유효성 검사 수정
    - 비밀번호 수정 시 비밀번호 확인 부분 에러 메시지가 업데이트 되지 않는 문제를 해결
    - 컴포넌트에서도 trigger가 필요하게 됨

--- kakaobase/src/components/user/PasswordStep.tsx
+++ kakaobase/src/components/user/PasswordStep.tsx
@@ -1,5 +1,5 @@
 import UserInput from '../inputs/UserInput';
-import { FieldErrors, UseFormRegister } from 'react-hook-form';
+import { FieldErrors, UseFormRegister, UseFormTrigger } from 'react-hook-form';
 import { z } from 'zod';
 import { passwordConfirmSchema } from '@/schemas/passwordConfirmSchema';
 
@@ -8,9 +8,14 @@ type PasswordStepFormData = z.infer<typeof passwordConfirmSchema>;
 interface PasswordStepProps {
   register: UseFormRegister<PasswordStepFormData>;
   errors: FieldErrors<PasswordStepFormData>;
+  trigger: UseFormTrigger<PasswordStepFormData>;
 }
 
-export default function PasswordStep({ register, errors }: PasswordStepProps) {
+export default function PasswordStep({
+  register,
+  errors,
+  trigger,
+}: PasswordStepProps) {
   return (
     <div className="flex flex-col gap-6">
       <UserInput
@@ -18,7 +23,11 @@ export default function PasswordStep({ register, errors }: PasswordStepProps) {
         placeholder="비밀번호를 입력하세요."
         type="password"
         errorMessage={errors.password?.message || ''}
-        {...register('password')}
+        {...register('password', {
+          onChange: () => {
+            trigger('confirm');
+          },
+        })}
       />
       <UserInput
         label="비밀번호 확인"
