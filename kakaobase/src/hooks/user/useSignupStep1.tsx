acad9f8292450e636bccc9a52f1522d5503068a1
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-11 04:26:43 +0900

    [REFACTOR] 파일 분리
    - 컴포넌트에 비즈니스 로직 들어간 부분 분리
    - useSignupStep1.tsx 파일 추가

---
+++ kakaobase/src/hooks/user/useSignupStep1.tsx
@@ -0,0 +1,19 @@
+import { useAuthStore } from '@/stores/emailAuthStore';
+import { useSignupStore } from '@/stores/signupStore';
+import { useRouter } from 'next/navigation';
+
+export const useSignupStep1 = () => {
+  const { isVerified, email } = useAuthStore();
+  const { setStep1 } = useSignupStore();
+  const router = useRouter();
+
+  const onSubmitStep1 = (password: string) => {
+    setStep1({ email, password });
+    router.push('/signup/step2');
+  };
+
+  return {
+    isVerified,
+    onSubmitStep1,
+  };
+};
