eaf26a78cb085f4c254f7bb8faca1fc309431085
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-12 05:43:24 +0900

    [FIX] 좋아요 등록 api 연결 성공
    - 게시글 좋아요 등록/취소 성공
    - 답글 삭제와 마찬가지로 답글, 대댓글 좋아요 등록 취소가 불가능

--- kakaobase/src/hooks/user/useLikeHook.tsx
+++ kakaobase/src/hooks/user/useLikeHook.tsx
@@ -1,19 +1,42 @@
+import {
+  deleteCommentLike,
+  deletePostLike,
+  likeComment,
+  likePost,
+} from '@/apis/like';
 import { useState } from 'react';
 
-export function useLikeToggle(initial: boolean, likeCnt: number) {
+export function useLikeToggle(
+  initial: boolean,
+  likeCnt: number,
+  id: number,
+  type: string
+) {
   const [isLiked, setLiked] = useState(initial);
   const [likeCount, setLikeCount] = useState(likeCnt);
 
   const toggleLike = async () => {
     try {
-      if (isLiked) {
-        // 좋아요 취소 api
-        setLikeCount((prev) => prev - 1);
-      } else {
-        // 좋아요 등록 api
-        setLikeCount((prev) => prev + 1);
+      let reponse = {};
+      if (type === 'post') {
+        if (isLiked) {
+          reponse = await deletePostLike({ id });
+          setLikeCount((prev) => prev - 1);
+        } else {
+          reponse = await likePost({ id });
+          setLikeCount((prev) => prev + 1);
+        }
+      } else if (type === 'comment') {
+        if (isLiked) {
+          const reponse = await deleteCommentLike({ id });
+          setLikeCount((prev) => prev - 1);
+        } else {
+          const reponse = await likeComment({ id });
+          setLikeCount((prev) => prev + 1);
+        }
       }
       setLiked(!isLiked);
+      console.log(reponse);
     } catch (e) {
       console.error('팔로우 토글 실패', e);
     }
