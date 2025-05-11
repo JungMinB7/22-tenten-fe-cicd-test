48ef62d411f5b9fd9ce8b4c9ac3d5c1cfc82819b
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-12 05:30:33 +0900

    [FIX] 댓글 삭제
    - 현재 댓글 삭제 구현 불가
    - 댓글의 type이 post로 돼있음
    - post.tsx ) console 출력 문구 제거

--- kakaobase/src/hooks/post/useDeleteHook.tsx
+++ kakaobase/src/hooks/post/useDeleteHook.tsx
@@ -1,10 +1,11 @@
+import { deleteComment } from '@/apis/comment';
 import { deletePost } from '@/apis/post';
 import { getClientCookie } from '@/lib/getClientCookie';
 import { PostType } from '@/lib/postType';
 import { usePathname, useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-export function useDeleteHook({ id }: { id: number }) {
+export function useDeleteHook({ id, type }: { id: number; type: string }) {
   let postType = getClientCookie('course') as PostType;
   if (!postType) postType = 'ALL';
 
@@ -13,10 +14,16 @@ export function useDeleteHook({ id }: { id: number }) {
   const [isOpened, setOpen] = useState(false);
   async function deletePostExecute() {
     try {
-      await deletePost({ postType, id });
+      if (type === 'post') await deletePost({ postType, id });
+      else if (type === 'comment') await deleteComment({ id });
+      // else await deleteRecomment({id}); // 대댓글 삭제
       setOpen(false);
-      if (path.includes('post')) router.push('/');
-      else window.location.reload(); //얘도 나중에 바꿔야 함
+      if (path.includes('post') && type === 'post')
+        router.push('/'); //게시글 상세에서 게시글 지우기
+      else if (path.includes('comment') && type === 'comment')
+        router.back(); //댓글 상세에서 댓글 지우기
+      else window.location.reload(); //댓글 상세에서 대댓글 지우기
+      //얘도 나중에 바꿔야 함
     } catch (e: any) {
       console.log(e);
     }
