f2dc20649c01a7c04c5764a455aec2aa7f404c7f
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-12 01:38:42 +0900

    [FIX] 답글 목록 조회 api 수정
    - commentList.tsx
    - postType은 쿠키에서 꺼내 쓰도록 수정

--- kakaobase/src/apis/commentList.tsx
+++ kakaobase/src/apis/commentList.tsx
@@ -1,8 +1,6 @@
 import { PostState } from '@/stores/postStore';
 import api from './api';
 import { getClientCookie } from '@/lib/getClientCookie';
-import { courseMap } from '@/lib/courseMap';
-import { PostType } from '@/lib/postType';
 import { mapToPostState } from '@/lib/mapPost';
 import { GetPostsParams } from './postList';
 
@@ -11,10 +9,8 @@ export default async function getComments(
   { limit, cursor }: GetPostsParams
 ): Promise<PostState[]> {
   try {
-    // let course = getClientCookie('course');
-    // if (!course) course = '기타 사용자';
-    // const postType = courseMap[course] as PostType;
-    const postType = 'PANGYO_2';
+    let postType = getClientCookie('course');
+    if (!postType) postType = '기타 사용자';
 
     const params: Record<string, any> = {};
     if (limit !== undefined) params.limit = limit;
