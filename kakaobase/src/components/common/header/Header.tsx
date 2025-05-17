0ca4d40afddea7068c7acebe45803777a100ac84
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-18 02:14:26 +0900

    [DESIGN] header position fixed
    - header position fixed로 변경
    - layout.tsx에서 아이템들이 세로 가운데 되도록 justify-center 추가

--- kakaobase/src/components/common/header/Header.tsx
+++ kakaobase/src/components/common/header/Header.tsx
@@ -2,7 +2,7 @@ import HeaderLeft from './HeaderLeft';
 
 export default function Header({ label }: { label: string }) {
   return (
-    <div className="px-6 py-6 flex relative text-textColor z-50 border-b-[1px] border-textOpacity50">
+    <div className="flex fixed py-6 gap-2 px-6 items-center w-full max-w-[420px] border-b-[1px] top-0 mx-auto lg:self-start bg-bgColor text-textColor shadow-sm">
       <HeaderLeft />
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-2xl">
         {label}
--- kakaobase/src/components/common/header/HeaderMain.tsx
+++ kakaobase/src/components/common/header/HeaderMain.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function HeaderMain() {
   return (
-    <div className="flex px-6 gap-2 py-2 items-center bg-bgColor border-b-[1px] border-textOpacity50">
+    <div className="flex fixed py-2 gap-2 px-6 items-center w-full max-w-[420px] border-b-[1px] top-0 mx-auto lg:self-start bg-bgColor text-textColor shadow-sm">
       <Image src="/logo_square.svg" width={64} height={64} alt="로고" />
       <div className="text-textColor flex flex-col">
         <div className="text-lg font-bold">KakaoBase</div>
