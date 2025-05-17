9ea4de3839d72e2f4f785e24269cc0439015fd7e
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-18 01:25:00 +0900

    [FIX] 유튜브 업로드 수정
    - 모바일 url 업로드 가능하게 함
    - id formatting하는 함수 조건을 모바일, 모바일 쇼츠, 일반, 일반 쇼츠로 구분

--- kakaobase/src/lib/formatYoutube.tsx
+++ kakaobase/src/lib/formatYoutube.tsx
@@ -1,7 +1,13 @@
 export function extractYoutubeVideoId(url: string): string | null {
   try {
     const parsedUrl = new URL(url);
-    return parsedUrl.searchParams.get('v');
+    if (parsedUrl.pathname.startsWith('/shorts/')) {
+      return parsedUrl.pathname.split('/')[2];
+    } else if (parsedUrl.hostname.includes('youtube.com')) {
+      return parsedUrl.searchParams.get('v');
+    } else {
+      return parsedUrl.pathname.split('/')[1];
+    }
   } catch (e) {
     return null;
   }
