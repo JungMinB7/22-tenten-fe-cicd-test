4ba538922653b8d3a70253cb0ec1c17cafddbb3c
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-22 16:54:37 +0900

    [CHORE] 회원가입2단계 수정
    - 스타일 속성을 잘못 추가해서 다시 수정

--- kakaobase/src/app/signup/step2/page.tsx
+++ kakaobase/src/app/signup/step2/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="overflow-y-auto">
       <Header label="회원가입" />
-      <div className="overflow-y-auto">
+      <div className="mt-20">
         <SignupStep2Form />
       </div>
     </div>
