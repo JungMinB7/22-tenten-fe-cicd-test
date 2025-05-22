bc086d369e326adb54fafbb914079e7de575896d
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-22 22:04:39 +0900

    [DESIGN] 삭제 모달 변경
    - 아이콘 교체/삭제 버튼 재클릭 방식으로 변경
    - 삭제/취소 버튼 ui 겹쳐서 코드 중복 줄이기 위해 컴포넌트 별도 추가

---
+++ kakaobase/src/components/common/SubmitButtonSmall2.tsx
@@ -0,0 +1,16 @@
+export default function SubmitButtonSmall2({
+  label,
+  execute,
+}: {
+  label: string;
+  execute: () => void;
+}) {
+  return (
+    <button
+      onClick={execute}
+      className="cursor-pointer bg-myLightBlue text-textOnLight px-4 rounded-full text-xs border-[1px] border-textOpacity50 hover:bg-myBlue hover:text-textOnBlue"
+    >
+      {label}
+    </button>
+  );
+}
