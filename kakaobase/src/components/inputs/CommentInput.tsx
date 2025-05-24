bc7873c8a79473b0f4afa3999251ef2595da57e2
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-05-25 08:16:44 +0900

    [FIX] 댓글 제출 한 번만 발생하도록
    - div 태그를 form 태그로 변경
    - 삭제 버튼 테두리 지움
    - 전역 상태로 댓글/대댓글 추가 함수 작성했지만 아직 window.location.reload() 문제를 구조 상 해결하지 못하여 작성 및 주석 처리

--- kakaobase/src/components/inputs/CommentInput.tsx
+++ kakaobase/src/components/inputs/CommentInput.tsx
@@ -7,7 +7,10 @@ export default function CommentInput() {
   return (
     <form
       className="flex fixed w-full max-w-[480px] border-t-[1px] border-textOpacity50 bottom-0 mx-auto lg:self-start bg-bgColor text-textColor shadow-md"
-      onSubmit={handleSubmit}
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
     >
       <div className="rounded-full mx-6 my-4 px-4 py-2 bg-containerColor w-full">
         <div className="w-full flex justify-between gap-2">
