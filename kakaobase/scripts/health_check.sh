bcafd3d3c8c34a5cff25fa917ab9e7a36505623d
JungMin <ljlm0829@gmail.com>
2025-06-04 21:31:41 +0900

    deploy.sh fix

--- kakaobase/scripts/health_check.sh
+++ kakaobase/scripts/health_check.sh
@@ -3,7 +3,7 @@ PORT=3000
 for i in {1..10}; do
 	echo "⌛ Waiting for container to warm up..."
 	sleep 5
-  STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:$PORT)
+  STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
   echo "Health check status: $STATUS"
   if [ "$STATUS" == "200" ]; then
     echo "✔ Health check passed."
