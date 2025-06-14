216236720cf24a0a8ab10e0cf64cb232f9c58e3d
JungMin <ljlm0829@gmail.com>
2025-06-14 20:13:42 +0900

    fix

--- kakaobase/scripts/deploy.sh
+++ kakaobase/scripts/deploy.sh
@@ -24,7 +24,7 @@ sudo docker run -d \
   --name frontend \
   -p 3000:3000 \
   --restart always \
-  --env NEXT_PUBLIC_API_URL=$PROD_API_URL \
+  --env NEXT_PUBLIC_API_URL="$PROD_API_URL" \
   --env NODE_ENV=production \
   --log-opt max-size=10m \
   --log-opt max-file=3 \
