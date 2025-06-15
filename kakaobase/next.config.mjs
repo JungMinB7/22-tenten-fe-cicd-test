37dbc712bc17a55f81c980bc004deec5136087c7
JungMin <ljlm0829@gmail.com>
2025-06-15 20:48:11 +0900

    error

--- kakaobase/next.config.mjs
+++ kakaobase/next.config.mjs
@@ -1,16 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ['s3-kakaobase-bucket.s3.ap-northeast-2.amazonaws.com'],
-//   },
-//   experimental: {
-//     logging: true,
-//   },
-// };
-
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -19,15 +6,28 @@ const nextConfig = {
   experimental: {
     logging: true,
   },
-  rewrites: async () => [
-    {
-      source: '/api/:path*',
-      destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
-    },
-  ],
 };
 
+
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['s3-kakaobase-bucket.s3.ap-northeast-2.amazonaws.com'],
+//   },
+//   experimental: {
+//     logging: true,
+//   },
+//   rewrites: async () => [
+//     {
+//       source: '/api/:path*',
+//       destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+//     },
+//   ],
+// };
+
+// export default nextConfig;
 
 
 
